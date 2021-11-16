b6cc74f22f6263a290f462ca7ee8b1530b1c7477
R Sowjanya <60657099+sowjanya-105@users.noreply.github.com>
2021-11-16 23:01:17 +0530

    Add files via upload

---
+++ src/data.jsx
@@ -0,0 +1,78 @@
+export const sliderItems = [
+   {
+    id: 1,
+    img: "https://img.freepik.com/free-photo/full-length-body-size-view-her-she-nice-attractive-lovely-ecstatic-cheerful-wavy-haired-girl-jumping-having-fun-leisure-vacation-isolated-bright-vivid-shine-vibrant-yellow-color-background_274222-31403.jpg?size=626&ext=jpg&ga=GA1.2.332967640.1630454400",
+    title: "WINTER COLLECTION",
+    desc: "DISCOVER OUR UNIQUE COLLECTIONS FOR THE BEST PRICE.",
+    bg: "FEBA02",
+   },
+
+    {
+      id: 2,
+      img: "http://cdn.wallpapername.com/2560x1600/20130420/women%20minimalistic%20redheads%20models%20fashion%20freckles%20cintia%20dicker%20simple%20background%20fashion%20model_www.wallpapername.com_40.jpg",
+      title: "VACAY SALE",
+      desc: "DISCOVER OUR UNIQUE COLLECTIONS FOR THE BEST PRICE.",
+      bg: "F278A1",
+    },
+
+    {
+      id: 3,
+      img: "http://www.leathercelebrities.com/images/uploads/Emily_DiDonato_photoshot_Fall_Winter_2014_Calzedonia_Collection041.jpg",
+      title: "CLASSIC WEAR",
+      desc: "DISCOVER OUR UNIQUE COLLECTIONS FOR THE BEST PRICE.",
+      bg: "fff",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.unsplash.com/photo-1574773065445-a838edfb1e4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      title: "STYLISH JACKETS!",
+    },
+    {
+      id: 2,
+      img: "https://images.unsplash.com/photo-1570212773364-e30cd076539e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1887&q=80",
+      title: "GORGEO ETHNICS!",
+    },
+    {
+      id: 3,
+      img: "https://images.unsplash.com/photo-1522001947148-8b4dfe064edc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=446&q=80",
+      title: "TRENDY ACCESSORIES",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://www.prada.com/content/dam/pradanux_products/1/1GG/1GG141/2DWZF0106/1GG141_2DWZ_F0106_SLD.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.800.1000.webp",
+    },
+    {
+      id:2,
+      img:"https://www.prada.com/content/dam/pradanux_products/V/VT6/VT6060/1ZR3F0009/VT6060_1ZR3_F0009_S_211_SLF.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.800.1000.webp",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/P/P3G/P3G23R/1Z4VF0002/P3G23R_1Z4V_F0002_S_221_SLF.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.800.1000.webp",
+    },
+    {
+      id:4,
+      img:"https://www.prada.com/content/dam/pradanux_products/1/138/138585/1ZTJF040X/138585_1ZTJ_F040X_S_212_SLF.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.800.1000.webp",
+    },
+    {
+      id:5,
+      img:"https://www.prada.com/content/dam/pradanux_products/S/SPR/SPR14W/E01RF00A6/SPR14W_E01R_F00A6_C_052_SLF.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.800.1000.webp",
+    },
+    {
+      id:6,
+      img:"https://www.prada.com/content/dam/pradanux_products/P/P12/P122T/1ZMMF0002/P122T_1ZMM_F0002_S_212_SLF.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.800.1000.webp",
+    },
+    {
+      id:7,
+      img:"https://www.prada.com/content/dam/pradanux_products/P/P44/P448FR/1Z4VF0106/P448FR_1Z4V_F0106_S_221_SLF.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.800.1000.webp",
+    },
+    {
+      id:8,
+      img:"https://www.prada.com/content/dam/pradanux_products/1/1X7/1X774M/2AWLF0T7O/1X774M_2AWL_F0T7O_F_090_SLR.png/_jcr_content/renditions/cq5dam.web.hf7f7f7.800.1000.webp",
+    },
+  ]
---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Slider from "../components/Slider";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+const Home = () => {
+    return (
+        <div>
+           <Announcement />
+           <Navbar />
+           <Slider />
+           <Categories />
+           <Products />
+           <Newsletter />
+           <Footer />
+        </div>
+    )
+}
+export default Home;
